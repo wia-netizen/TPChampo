9f73f128a7b5b77e52e2dd35e8e92ce164462ca9
Rémi Bastide <Remi.Bastide@gmail.com>
2020-11-17 18:35:22 +0100

    JUnit 5 - résultats entiers

--- src/test/java/champollion/ChampollionJUnitTest.java
+++ src/test/java/champollion/ChampollionJUnitTest.java
@@ -1,19 +1,13 @@
 package champollion;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.*;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- * @author rbastide
- */
 public class ChampollionJUnitTest {
 	Enseignant untel;
 	UE uml, java;
 		
-	@Before
+	@BeforeEach
 	public void setUp() {
 		untel = new Enseignant("untel", "untel@gmail.com");
 		uml = new UE("UML");
@@ -23,18 +17,23 @@ public class ChampollionJUnitTest {
 
 	@Test
 	public void testNouvelEnseignantSansService() {
-		// Le dernier paramètre représente la "marge d'erreur" acceptable
-		assertEquals(0, untel.heuresPrevues(), 0.001f);
+		assertEquals(0, untel.heuresPrevues(),
+                        "Un nouvel enseignant doit avoir 0 heures prévues");
 	}
 	
 	@Test
 	public void testAjouteHeures() {
+                // 10h TD pour UML
 		untel.ajouteEnseignement(uml, 0, 10, 0);
-		// Il a maintenant 10 heures prévues pour cette UE
-		assertEquals(10, untel.heuresPrevuesPourUE(uml), 0.001f);
-		untel.ajouteEnseignement(uml, 0, 20, 0);
-		// Il a maintenant 10 + 20 heures prévues pour cette UE
-		assertEquals(10 + 20, untel.heuresPrevuesPourUE(uml), 0.001f);		
+
+		assertEquals(10, untel.heuresPrevuesPourUE(uml),
+                        "L'enseignant doit maintenant avoir 10 heures prévues pour l'UE 'uml'");
+
+                // 20h TD pour UML
+                untel.ajouteEnseignement(uml, 0, 20, 0);
+                
+		assertEquals(10 + 20, untel.heuresPrevuesPourUE(uml),
+                         "L'enseignant doit maintenant avoir 30 heures prévues pour l'UE 'uml'");		
 		
 	}
 	
